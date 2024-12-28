40b54291cd76701e015d79738b310fa740975d3f
Hrishikesh <hrishithombare15@gmail.com>
2024-12-28 19:49:50 +0530

    Added new stats

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -503,7 +503,7 @@ commands = {
     "yt-dlp": (["yt-dlp", "--version"], r"([\d.]+)"),
     "ffmpeg": (["ffmpeg", "-version"], r"ffmpeg version ([\d.]+(-\w+)?).*"),
     "7z": (["7z", "-version"], r"7-Zip ([\d.]+)"),
-    "mega": ("pip3", "show", "megasdk", r"Version:\s*([\d\.]+)"),
+    "mega": (["pip3", "show", "megasdk"], r"Version:\s*([\d\.]+)"),
     "pyrogram":(["pip3", "show", "pyrofork"], r"Pyrofork v[\s*([\d\.]+)]"),
     "tgcrypto":(["pip3", "show", "tgcrypto"], r"TgCrypto v[\s*([\d\.]+)]")}
 
